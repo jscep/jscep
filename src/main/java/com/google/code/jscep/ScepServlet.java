9d8c3db19816b083b3d9fcb1e9ad19b7aa9b44eb
David Grant <david@grant.org.uk>
2009-12-04 09:39:36 +0000

    Added Tests

---
+++ src/main/java/com/google/code/jscep/ScepServlet.java
@@ -0,0 +1,23 @@
+package com.google.code.jscep;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ScepServlet extends HttpServlet {
+
+	/**
+	 * Serialization ID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}
--- src/main/java/com/google/code/jscep/asn1/IssuerAndSubject.java
+++ src/main/java/com/google/code/jscep/asn1/IssuerAndSubject.java
@@ -40,37 +40,75 @@ import org.bouncycastle.asn1.x509.X509Name;
  * TODO: BC Dependency
  */
 public class IssuerAndSubject implements DEREncodable {
-    private final X509Name issuer;
-    private final X509Name subject;
+	private final X509Name issuer;
+	private final X509Name subject;
 
-    public IssuerAndSubject(ASN1Sequence seq) {
-        this.issuer = (X509Name) seq.getObjectAt(0);
-        this.subject = (X509Name) seq.getObjectAt(1);
-    }
+	public IssuerAndSubject(ASN1Sequence seq) {
+		this.issuer = (X509Name) seq.getObjectAt(0);
+		this.subject = (X509Name) seq.getObjectAt(1);
+	}
 
-    public IssuerAndSubject(X509Name issuer, X509Name subject) {
-        this.issuer = issuer;
-        this.subject = subject;
-    }
+	public IssuerAndSubject(X509Name issuer, X509Name subject) {
+		this.issuer = issuer;
+		this.subject = subject;
+	}
 
-    public DERObject getDERObject() {
-        ASN1EncodableVector v = new ASN1EncodableVector();
+	public DERObject getDERObject() {
+		ASN1EncodableVector v = new ASN1EncodableVector();
 
-        v.add(issuer);
-        v.add(subject);
+		v.add(issuer);
+		v.add(subject);
 
-        return new DERSequence(v);
-    }
+		return new DERSequence(v);
+	}
 
-    public X509Name getIssuer() {
-        return issuer;
-    }
+	public X509Name getIssuer() {
+		return issuer;
+	}
 
-    public X509Name getSubject() {
-        return subject;
-    }
-    
-    public byte[] getEncoded() throws IOException {
-    	return getDERObject().getEncoded();
-    }
+	public X509Name getSubject() {
+		return subject;
+	}
+
+	public byte[] getEncoded() throws IOException {
+		return getDERObject().getEncoded();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((issuer == null) ? 0 : issuer.hashCode());
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		IssuerAndSubject other = (IssuerAndSubject) obj;
+		if (issuer == null) {
+			if (other.issuer != null) {
+				return false;
+			}
+		} else if (!issuer.equals(other.issuer)) {
+			return false;
+		}
+		if (subject == null) {
+			if (other.subject != null) {
+				return false;
+			}
+		} else if (!subject.equals(other.subject)) {
+			return false;
+		}
+		return true;
+	}
 }
