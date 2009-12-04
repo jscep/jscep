8453cf35eba6ed243b8dd90e44dacff2b4c9a194
David Grant <david@grant.org.uk>
2009-12-04 10:29:55 +0000

    Added Jetty for establishing tests.

--- src/main/java/com/google/code/jscep/ScepServlet.java
+++ src/main/java/com/google/code/jscep/ScepServlet.java
@@ -1,11 +1,13 @@
 package com.google.code.jscep;
 
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ScepServlet extends HttpServlet {
-
+	private static final Logger LOGGER = Logger.getLogger(ScepServlet.class.getName());
 	/**
 	 * Serialization ID
 	 */
@@ -13,11 +15,11 @@ public class ScepServlet extends HttpServlet {
 	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse res) {
-		
+		LOGGER.info("doPost()");
 	}
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) {
-		
+		LOGGER.info("doGet()");
 	}
 }
