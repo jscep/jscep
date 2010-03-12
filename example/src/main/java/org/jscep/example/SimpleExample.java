b64836fe0f13d8edb7434c37bef76115dfa8b5e8
David Grant <david@grant.org.uk>
2010-03-12 12:49:34 +0000

    Added example.

---
+++ example/src/main/java/org/jscep/example/SimpleExample.java
@@ -0,0 +1,60 @@
+package org.jscep.example;
+
+import java.io.Console;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.jscep.FingerprintVerificationCallback;
+import org.jscep.client.Client;
+
+public class SimpleExample {
+	public static void main(String[] args) throws Exception {
+		final KeyStore store = KeyStore.getInstance("JKS");
+		store.load(new FileInputStream("src/main/resources/example.jks"), "jscep.org".toCharArray());
+		
+		final URL url = new URL("http://jscep.org/scep/pkiclient.exe");
+		final X509Certificate identity = (X509Certificate) store.getCertificate("jscep");
+		final PrivateKey privKey = (PrivateKey) store.getKey("jscep", "jscep.org".toCharArray());
+		final CallbackHandler cbh = new ConsoleCallbackHandler();
+		
+		final Client scepClient = new Client(url, identity, privKey, cbh);
+		final List<X509Certificate> certs = scepClient.getCaCertificate();
+	}
+	
+	private static class ConsoleCallbackHandler implements CallbackHandler {
+		public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+			for (int i = 0; i < callbacks.length; i++) {
+				if (callbacks[i] instanceof FingerprintVerificationCallback) {
+					final FingerprintVerificationCallback callback = (FingerprintVerificationCallback) callbacks[i];
+					final byte[] fingerprint = callback.getFingerprint();
+					final Console console = System.console();
+					if (console == null) {
+						continue;
+					}
+					final PrintWriter writer = console.writer();
+					writer.write("Is this the " + callback.getAlgorithm() + " hash of your CA's certificate?\n");
+					writer.write(new String(fingerprint) + "\n");
+					writer.write("[yes/no]: ");
+					writer.flush();
+					final String reply = console.readLine();
+					if (reply.equals("yes")) {
+						callback.setVerified(true);
+					}
+				} else {
+					throw new UnsupportedCallbackException(callbacks[i]);
+				}
+			}
+		}
+		
+	}
+}
