1b7bc8730ab3ec2bd441bc3be134b49242bbf208
David Grant <david@grant.org.uk>
2012-06-27 13:13:13 +0000

    Added console callback handler.

---
+++ src/main/java/org/jscep/client/ConsoleCallbackHandler.java
@@ -0,0 +1,90 @@
+package org.jscep.client;
+
+import static java.security.Security.getAlgorithms;
+import static org.jscep.util.HexUtil.toHexString;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.jscep.CertificateVerificationCallback;
+
+/**
+ * This class is a console-focused implementation of CallbackHandler.
+ * <p>
+ * Implementors should use this if they require interactive certificate
+ * verification in a command line application.
+ */
+public class ConsoleCallbackHandler implements CallbackHandler {
+    /**
+     * {@inheritDoc}
+     */
+    public final void handle(final Callback[] callbacks) throws IOException,
+            UnsupportedCallbackException {
+        for (final Callback callback : callbacks) {
+            if (callback instanceof CertificateVerificationCallback) {
+                try {
+                    final CertificateVerificationCallback cert = (CertificateVerificationCallback) callback;
+                    cert.setVerified(verify(cert.getCertificate()));
+                } catch (final GeneralSecurityException e) {
+                    final IOException ioe = new IOException();
+                    ioe.initCause(e);
+
+                    throw ioe;
+                }
+            }
+        }
+    }
+
+    /**
+     * Use the system console to prompt the user with each available hash.
+     * 
+     * @param cert the certificate to verify
+     * @return <tt>true</tt> if the certificate is verified, <tt>false</tt>
+     *         otherwise.
+     * @throws GeneralSecurityException if any security error occurs
+     */
+    private boolean verify(final X509Certificate cert)
+            throws GeneralSecurityException {
+        final List<String> algs = new ArrayList<String>(
+                getAlgorithms(MessageDigest.class.getSimpleName()));
+        Collections.sort(algs);
+        int max = 0;
+        for (final String alg : algs) {
+            if (alg.length() > max) {
+                max = alg.length();
+            }
+        }
+        for (final String alg : algs) {
+            final MessageDigest digest = MessageDigest.getInstance(alg);
+            final byte[] hash = digest.digest(cert.getEncoded());
+            System.out.format("%" + max + "s: %s%n", alg, toHexString(hash));
+        }
+        final Scanner scanner = new Scanner(System.in).useDelimiter(String
+                .format("%n"));
+        while (true) {
+            System.out.format("%nAccept? (Y/N): [N]%n");
+            try {
+                final String answer = scanner.next();
+                System.out.println(answer);
+                if (answer.equals("Y")) {
+                    return true;
+                } else if (answer.equals("N")) {
+                    return false;
+                }
+            } catch (final NoSuchElementException e) {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/test/java/org/jscep/client/ConsoleCallbackHandlerTest.java
@@ -0,0 +1,82 @@
+package org.jscep.client;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.cert.X509Certificate;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.x500.X500Principal;
+
+import org.jscep.CertificateVerificationCallback;
+import org.jscep.x509.X509Util;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+
+public class ConsoleCallbackHandlerTest {
+    private X509Certificate cert;
+    private ConsoleCallbackHandler handler;
+
+    @Before
+    public void setUp() throws GeneralSecurityException {
+        X500Principal subject = new X500Principal("CN=example");
+        KeyPair keyPair = KeyPairGenerator.getInstance("RSA").genKeyPair();
+        cert = X509Util.createEphemeralCertificate(subject, keyPair);
+        handler = new ConsoleCallbackHandler();
+    }
+
+    @Test
+    public void testYesResponse() throws Exception {
+        CertificateVerificationCallback callback = new CertificateVerificationCallback(
+                cert);
+
+        byte[] bytes = String.format("Y%n").getBytes(Charsets.US_ASCII.name());
+        System.setIn(new ByteArrayInputStream(bytes));
+        handler.handle(new Callback[] { callback });
+
+        assertTrue(callback.isVerified());
+    }
+
+    @Test
+    public void testEmptyResponse() throws Exception {
+        CertificateVerificationCallback callback = new CertificateVerificationCallback(
+                cert);
+
+        byte[] bytes = String.format("%n").getBytes(Charsets.US_ASCII.name());
+        System.setIn(new ByteArrayInputStream(bytes));
+        handler.handle(new Callback[] { callback });
+
+        assertFalse(callback.isVerified());
+    }
+
+    @Test
+    public void testNoResponse() throws Exception {
+        CertificateVerificationCallback callback = new CertificateVerificationCallback(
+                cert);
+
+        byte[] bytes = String.format("N%n").getBytes(Charsets.US_ASCII.name());
+        System.setIn(new ByteArrayInputStream(bytes));
+        handler.handle(new Callback[] { callback });
+
+        assertFalse(callback.isVerified());
+    }
+
+    @Test
+    public void testInvalidResponse() throws Exception {
+        CertificateVerificationCallback callback = new CertificateVerificationCallback(
+                cert);
+
+        byte[] bytes = String.format("X%nY%n").getBytes(
+                Charsets.US_ASCII.name());
+        System.setIn(new ByteArrayInputStream(bytes));
+        handler.handle(new Callback[] { callback });
+
+        assertTrue(callback.isVerified());
+    }
+}
