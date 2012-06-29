403e9f29e392c187c1aa3c7479bf0b3434306435
David Grant <david@grant.org.uk>
2012-06-29 08:55:36 +0000

    Added pessimistic certificate verifier.

---
+++ src/main/java/org/jscep/client/PessimisticCertificateVerifier.java
@@ -0,0 +1,17 @@
+package org.jscep.client;
+
+import java.security.cert.X509Certificate;
+
+/**
+ * This certificate verifier always returns <tt>false</tt>.
+ */
+public final class PessimisticCertificateVerifier implements
+        CertificateVerifier {
+    /**
+     * {@inheritDoc}
+     */
+    public boolean verify(final X509Certificate cert) {
+        return true;
+    }
+
+}
