0c8d53072fa28a310a710ebc1c2979ecf40becaf
David Grant <david@grant.org.uk>
2012-07-19 18:10:28 +0000

    Changed exception model further.

---
+++ src/main/java/org/jscep/client/ClientException.java
@@ -0,0 +1,16 @@
+package org.jscep.client;
+
+public class ClientException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public ClientException(Throwable cause) {
+        super(cause);
+    }
+
+    public ClientException(String message) {
+        super(message);
+    }
+}
