6e794a46fc5966c579080b811c78305b01009570
David Grant <david@grant.org.uk>
2012-08-11 20:49:39 +0000

    Fixed failing unit tests.

--- src/test/java/org/jscep/client/ScepServerSupport.java
+++ src/test/java/org/jscep/client/ScepServerSupport.java
@@ -5,26 +5,17 @@ import java.net.URL;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.jscep.server.ScepServletImpl;
-import org.junit.Before;
 
 public abstract class ScepServerSupport {
-	private static String PATH = "/scep/pkiclient.exe";
-	private Server server;
-	private URL url;
-	private int port;
-	
-	@Before
-	public void setUp() throws Exception {
+	public URL getUrl() throws Exception {
+		final String path = "/scep/pkiclient.exe";
 		final ServletHandler handler = new ServletHandler();
-		handler.addServletWithMapping(ScepServletImpl.class, PATH);
-		server = new Server(0);
+		handler.addServletWithMapping(ScepServletImpl.class, path);
+		final Server server = new Server(0);
 		server.setHandler(handler);
 		server.start();
-		port = server.getConnectors()[0].getLocalPort();
-		url = new URL("http", "localhost", port, PATH);
-	}
-	
-	public URL getUrl() {
-		return url;
+		
+		final int port = server.getConnectors()[0].getLocalPort();
+		return new URL("http", "localhost", port, path);
 	}
 }
