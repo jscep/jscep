661ba5c3813ce460b79a01e93adfec196d0e3289
David Grant <david@grant.org.uk>
2012-09-03 14:19:41 +0000

    Changed nonce queue to use weak references for caching.  Fixes issue 80.

--- src/main/java/org/jscep/transaction/NonceQueue.java
+++ src/main/java/org/jscep/transaction/NonceQueue.java
@@ -1,61 +1,33 @@
 package org.jscep.transaction;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 /**
  * This class provides support for detecting replay attacks.
- * <p/>
- * The size of this queue can be altered depending on memory requirements.
  */
 public final class NonceQueue {
-    /**
-     * The default size for a <tt>NonceQueue</tt>.
-     */
-    public static final int DEFAULT_QUEUE_SIZE = 20;
-    private static final Logger LOGGER = LoggerFactory
-	    .getLogger(NonceQueue.class);
-    private final int size;
-    private final Queue<Nonce> backingQueue;
+    private static final int DEFAULT_QUEUE_SIZE = 20;
+    private final Map<Nonce, Boolean> backingQueue;
 
     /**
-     * Creates a new <tt>NonceQueue</tt> of the specified size.
+     * Creates a new <tt>NonceQueue</tt> of the specified default size.
      * 
      * @param size
      *            the size of the queue.
      */
-    public NonceQueue(int size) {
-	this.size = size;
-	this.backingQueue = new LinkedList<Nonce>();
-    }
-
-    /**
-     * Creates a <tt>NonceQueue</tt> of a default size.
-     */
     public NonceQueue() {
-	this(DEFAULT_QUEUE_SIZE);
+	this.backingQueue = new WeakHashMap<Nonce, Boolean>(DEFAULT_QUEUE_SIZE);
     }
 
     /**
      * Inserts the specified <tt>Nonce</tt> into this queue.
-     * <p/>
-     * This queue will maintain a fixed size, pushing out the oldest <tt>Nonce</tt>
-     * first.
      * 
      * @param nonce
      *            the nonce to add.
      */
     public synchronized void add(final Nonce nonce) {
-	if (backingQueue.size() == size) {
-	    Nonce removedNonce = backingQueue.poll();
-	    if (LOGGER.isTraceEnabled()) {
-		LOGGER.trace("Removed {} from head of queue.", removedNonce);
-	    }
-	}
-	backingQueue.offer(nonce);
+	backingQueue.put(nonce, Boolean.FALSE);
     }
 
     /**
@@ -66,6 +38,6 @@ public final class NonceQueue {
      * @return <tt>true</tt> if the <tt>Nonce</tt> is present, <tt>false</tt> otherwise.
      */
     public synchronized boolean contains(final Nonce nonce) {
-	return backingQueue.contains(nonce);
+	return backingQueue.containsKey(nonce);
     }
 }
--- src/test/java/org/jscep/transaction/NonceQueueTest.java
+++ src/test/java/org/jscep/transaction/NonceQueueTest.java
@@ -7,25 +7,18 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class NonceQueueTest {
-    private static final int SIZE = 1;
     private NonceQueue queue;
     private Nonce nonce;
 
     @Before
     public void setUp() {
 	nonce = Nonce.nextNonce();
-	queue = new NonceQueue(SIZE);
+	queue = new NonceQueue();
 	queue.add(nonce);
     }
 
     @Test
     public void testQueueContainsOriginalNonce() {
 	assertThat(queue.contains(nonce), is(true));
-    }
-
-    @Test
-    public void testQueueDoesNotContainsOriginalNonceAfterSizeExceeded() {
-	queue.add(Nonce.nextNonce());
-	assertThat(queue.contains(nonce), is(false));
     }
 }
