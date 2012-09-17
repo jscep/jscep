a2a28e0f4facb4667040b54512e25752906f30f8
David Grant <david@grant.org.uk>
2012-09-17 11:32:38 +0000

    Manually set security provider to be Bouncy Castle when wrapping and unwrapping keys.
    Fixes issue 84.

--- src/main/java/org/jscep/message/PkcsPkiEnvelopeDecoder.java
+++ src/main/java/org/jscep/message/PkcsPkiEnvelopeDecoder.java
@@ -11,7 +11,9 @@ import org.bouncycastle.cms.CMSException;
 import org.bouncycastle.cms.RecipientInformation;
 import org.bouncycastle.cms.RecipientInformationStore;
 import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
+import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipientId;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 
 /**
@@ -21,73 +23,75 @@ import org.slf4j.Logger;
  * @see PkcsPkiEnvelopeEncoder
  */
 public final class PkcsPkiEnvelopeDecoder {
-    private static final Logger LOGGER = getLogger(PkcsPkiEnvelopeDecoder.class);
-    private final X509Certificate recipient;
-    private final PrivateKey priKey;
+	private static final Logger LOGGER = getLogger(PkcsPkiEnvelopeDecoder.class);
+	private final X509Certificate recipient;
+	private final PrivateKey priKey;
 
-    /**
-     * Creates a <tt>PkcsPkiEnveloperDecoder</tt> for the provided certificate
-     * and key.
-     * <p>
-     * The provided certificate is used to identify the envelope recipient info,
-     * which is then used with the key unwrapped using the provided key.
-     * 
-     * @param recipient
-     *            the entity for whom the message was enveloped.
-     * @param priKey
-     *            the key to unwrap the symmetric encrypting key.
-     */
-    public PkcsPkiEnvelopeDecoder(X509Certificate recipient, PrivateKey priKey) {
-	this.recipient = recipient;
-	this.priKey = priKey;
-    }
-
-    /**
-     * Decrypts the provided <tt>pkcsPkiEnvelope</tt>, and extracts the content.
-     * 
-     * @param pkcsPkiEnvelope
-     *            the envelope to decrypt and open.
-     * @return the content of the <tt>pkcsPkiEnvelope</tt>, the SCEP
-     *         <tt>messageData</tt>.
-     * @throws MessageDecodingException
-     *             if the envelope cannot be decoded.
-     */
-    public byte[] decode(CMSEnvelopedData pkcsPkiEnvelope)
-	    throws MessageDecodingException {
-	LOGGER.debug("Decoding pkcsPkiEnvelope");
-	validate(pkcsPkiEnvelope);
-
-	LOGGER.debug(
-		"Decrypting pkcsPkiEnvelope using key belonging to [issuer={}; serial={}]",
-		recipient.getIssuerDN(), recipient.getSerialNumber());
-	final RecipientInformationStore recipientInfos = pkcsPkiEnvelope
-		.getRecipientInfos();
-	RecipientInformation info = recipientInfos
-		.get(new JceKeyTransRecipientId(recipient));
-
-	if (info == null) {
-	    throw new MessageDecodingException(
-		    "Missing expected key transfer recipient");
+	/**
+	 * Creates a <tt>PkcsPkiEnveloperDecoder</tt> for the provided certificate
+	 * and key.
+	 * <p>
+	 * The provided certificate is used to identify the envelope recipient info,
+	 * which is then used with the key unwrapped using the provided key.
+	 * 
+	 * @param recipient
+	 *            the entity for whom the message was enveloped.
+	 * @param priKey
+	 *            the key to unwrap the symmetric encrypting key.
+	 */
+	public PkcsPkiEnvelopeDecoder(X509Certificate recipient, PrivateKey priKey) {
+		this.recipient = recipient;
+		this.priKey = priKey;
 	}
 
-	LOGGER.debug("pkcsPkiEnvelope encryption algorithm: {}",
-		info.getKeyEncryptionAlgorithm().getAlgorithm());
+	/**
+	 * Decrypts the provided <tt>pkcsPkiEnvelope</tt>, and extracts the content.
+	 * 
+	 * @param pkcsPkiEnvelope
+	 *            the envelope to decrypt and open.
+	 * @return the content of the <tt>pkcsPkiEnvelope</tt>, the SCEP
+	 *         <tt>messageData</tt>.
+	 * @throws MessageDecodingException
+	 *             if the envelope cannot be decoded.
+	 */
+	public byte[] decode(CMSEnvelopedData pkcsPkiEnvelope)
+			throws MessageDecodingException {
+		LOGGER.debug("Decoding pkcsPkiEnvelope");
+		validate(pkcsPkiEnvelope);
 
-	try {
-	    byte[] messageData = info
-		    .getContent(new JceKeyTransEnvelopedRecipient(priKey));
-	    LOGGER.debug("Finished decoding pkcsPkiEnvelope");
-	    return messageData;
-	} catch (CMSException e) {
-	    throw new MessageDecodingException(e);
+		LOGGER.debug(
+				"Decrypting pkcsPkiEnvelope using key belonging to [issuer={}; serial={}]",
+				recipient.getIssuerDN(), recipient.getSerialNumber());
+		final RecipientInformationStore recipientInfos = pkcsPkiEnvelope
+				.getRecipientInfos();
+		RecipientInformation info = recipientInfos
+				.get(new JceKeyTransRecipientId(recipient));
+
+		if (info == null) {
+			throw new MessageDecodingException(
+					"Missing expected key transfer recipient");
+		}
+
+		LOGGER.debug("pkcsPkiEnvelope encryption algorithm: {}", info
+				.getKeyEncryptionAlgorithm().getAlgorithm());
+
+		try {
+			JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(priKey);
+			recipient.setProvider(new BouncyCastleProvider());
+			
+			byte[] messageData = info.getContent(recipient);
+			LOGGER.debug("Finished decoding pkcsPkiEnvelope");
+			return messageData;
+		} catch (CMSException e) {
+			throw new MessageDecodingException(e);
+		}
 	}
-    }
 
-    private void validate(CMSEnvelopedData pkcsPkiEnvelope) {
-	EnvelopedData ed = EnvelopedData.getInstance(pkcsPkiEnvelope
-		.toASN1Structure().getContent());
-	LOGGER.debug("pkcsPkiEnvelope version: {}", ed.getVersion());
-	LOGGER.debug("pkcsPkiEnvelope encryptedContentInfo contentType: {}", ed
-		.getEncryptedContentInfo().getContentType());
-    }
+	private void validate(CMSEnvelopedData pkcsPkiEnvelope) {
+		EnvelopedData ed = EnvelopedData.getInstance(pkcsPkiEnvelope
+				.toASN1Structure().getContent());
+		LOGGER.debug("pkcsPkiEnvelope version: {}", ed.getVersion());
+		LOGGER.debug("pkcsPkiEnvelope encryptedContentInfo contentType: {}", ed
+				.getEncryptedContentInfo().getContentType());
+	}
 }
--- src/main/java/org/jscep/message/PkcsPkiEnvelopeEncoder.java
+++ src/main/java/org/jscep/message/PkcsPkiEnvelopeEncoder.java
@@ -12,6 +12,7 @@ import org.bouncycastle.cms.CMSTypedData;
 import org.bouncycastle.cms.RecipientInfoGenerator;
 import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.OutputEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,62 +24,64 @@ import org.slf4j.LoggerFactory;
  * @see PkcsPkiEnvelopeDecoder
  */
 public final class PkcsPkiEnvelopeEncoder {
-    private static final Logger LOGGER = LoggerFactory
-	    .getLogger(PkcsPkiEnvelopeEncoder.class);
-    private final X509Certificate recipient;
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(PkcsPkiEnvelopeEncoder.class);
+	private final X509Certificate recipient;
 
-    /**
-     * Creates a new <tt>PkcsPkiEnvelopeEncoder</tt> for the entity identified
-     * by the provided certificate.
-     * 
-     * @param recipient
-     *            the entity for whom the <tt>pkcsPkiEnvelope</tt> is intended.
-     */
-    public PkcsPkiEnvelopeEncoder(X509Certificate recipient) {
-	this.recipient = recipient;
-    }
+	/**
+	 * Creates a new <tt>PkcsPkiEnvelopeEncoder</tt> for the entity identified
+	 * by the provided certificate.
+	 * 
+	 * @param recipient
+	 *            the entity for whom the <tt>pkcsPkiEnvelope</tt> is intended.
+	 */
+	public PkcsPkiEnvelopeEncoder(X509Certificate recipient) {
+		this.recipient = recipient;
+	}
 
-    /**
-     * Encrypts and envelops the provided messageData.
-     * 
-     * @param messageData
-     *            the message data to encrypt and envelop.
-     * @return the enveloped data.
-     * @throws MessageEncodingException
-     *             if there are any problems encoding the message.
-     */
-    public CMSEnvelopedData encode(byte[] messageData)
-	    throws MessageEncodingException {
-	LOGGER.debug("Encoding pkcsPkiEnvelope");
-	CMSEnvelopedDataGenerator edGenerator = new CMSEnvelopedDataGenerator();
-	CMSTypedData envelopable = new CMSProcessableByteArray(messageData);
-	RecipientInfoGenerator recipientGenerator;
-	try {
-	    recipientGenerator = new JceKeyTransRecipientInfoGenerator(
-		    recipient);
-	} catch (CertificateEncodingException e) {
-	    throw new MessageEncodingException(e);
-	}
-	edGenerator.addRecipientInfoGenerator(recipientGenerator);
-	LOGGER.debug(
-		"Encrypting pkcsPkiEnvelope using key belonging to [issuer={}; serial={}]",
-		recipient.getIssuerDN(), recipient.getSerialNumber());
+	/**
+	 * Encrypts and envelops the provided messageData.
+	 * 
+	 * @param messageData
+	 *            the message data to encrypt and envelop.
+	 * @return the enveloped data.
+	 * @throws MessageEncodingException
+	 *             if there are any problems encoding the message.
+	 */
+	public CMSEnvelopedData encode(byte[] messageData)
+			throws MessageEncodingException {
+		LOGGER.debug("Encoding pkcsPkiEnvelope");
+		CMSEnvelopedDataGenerator edGenerator = new CMSEnvelopedDataGenerator();
+		CMSTypedData envelopable = new CMSProcessableByteArray(messageData);
+		RecipientInfoGenerator recipientGenerator;
+		try {
+			recipientGenerator = new JceKeyTransRecipientInfoGenerator(
+					recipient);
+		} catch (CertificateEncodingException e) {
+			throw new MessageEncodingException(e);
+		}
+		edGenerator.addRecipientInfoGenerator(recipientGenerator);
+		LOGGER.debug(
+				"Encrypting pkcsPkiEnvelope using key belonging to [issuer={}; serial={}]",
+				recipient.getIssuerDN(), recipient.getSerialNumber());
 
-	OutputEncryptor encryptor;
-	try {
-	    // TODO: Don't rely on using Triple DES
-	    encryptor = new JceCMSContentEncryptorBuilder(
-		    PKCSObjectIdentifiers.des_EDE3_CBC).build();
-	} catch (CMSException e) {
-	    throw new MessageEncodingException(e);
+		OutputEncryptor encryptor;
+		try {
+			// TODO: Don't rely on using Triple DES
+			encryptor = new JceCMSContentEncryptorBuilder(
+					PKCSObjectIdentifiers.des_EDE3_CBC).setProvider(
+					new BouncyCastleProvider()).build();
+		} catch (CMSException e) {
+			throw new MessageEncodingException(e);
+		}
+		try {
+			CMSEnvelopedData pkcsPkiEnvelope = edGenerator.generate(
+					envelopable, encryptor);
+
+			LOGGER.debug("Finished encoding pkcsPkiEnvelope");
+			return pkcsPkiEnvelope;
+		} catch (CMSException e) {
+			throw new MessageEncodingException(e);
+		}
 	}
-	try {
-	    CMSEnvelopedData pkcsPkiEnvelope = edGenerator.generate(envelopable, encryptor);
-	    
-	    LOGGER.debug("Finished encoding pkcsPkiEnvelope");
-	    return pkcsPkiEnvelope;
-	} catch (CMSException e) {
-	    throw new MessageEncodingException(e);
-	}
-    }
 }
