338fad4adbbd55bfc2bbc64a2ffa7b856aa8eaee
David Grant <david@grant.org.uk>
2012-09-21 12:36:21 +0000

    Updated documentation.

--- src/main/java/org/jscep/transaction/TransactionId.java
+++ src/main/java/org/jscep/transaction/TransactionId.java
@@ -16,101 +16,101 @@ import org.apache.commons.lang.ArrayUtils;
  * This class represents a SCEP <code>transactionID</code> attribute.
  */
 public final class TransactionId implements Serializable {
-    private static final long serialVersionUID = -5248125945726721520L;
-    private static final AtomicLong ID_SOURCE = new AtomicLong();
-    private final byte[] id;
+	private static final long serialVersionUID = -5248125945726721520L;
+	private static final AtomicLong ID_SOURCE = new AtomicLong();
+	private final byte[] id;
 
-    /**
-     * Creates a new <tt>TransactionId</tt> from the provided byte array.
-     * 
-     * @param id
-     *            the ID to copy.
-     */
-    public TransactionId(byte[] id) {
-	this.id = ArrayUtils.clone(id);
-    }
-
-    private TransactionId(PublicKey pubKey, String digestAlgorithm) {
-	MessageDigest digest;
-	try {
-	    digest = MessageDigest.getInstance(digestAlgorithm);
-	} catch (NoSuchAlgorithmException e) {
-	    throw new RuntimeException(e);
+	/**
+	 * Creates a new <tt>TransactionId</tt> from the provided byte array.
+	 * 
+	 * @param id
+	 *            the ID to copy.
+	 */
+	public TransactionId(byte[] id) {
+		this.id = ArrayUtils.clone(id);
 	}
-	id = new Hex().encode(digest.digest(pubKey.getEncoded()));
-    }
 
-    private TransactionId() {
-	try {
-	    id = Long.toHexString(ID_SOURCE.getAndIncrement()).getBytes(
-		    Charsets.US_ASCII.name());
-	} catch (UnsupportedEncodingException e) {
-	    throw new RuntimeException(e);
+	private TransactionId(PublicKey pubKey, String digestAlgorithm) {
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance(digestAlgorithm);
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		id = new Hex().encode(digest.digest(pubKey.getEncoded()));
 	}
-    }
 
-    /**
-     * Creates a new Transaction ID
-     * <p>
-     * Each call to this method will return the same transaction ID given the
-     * same parameters.
-     * 
-     * @param pubKey
-     *            the key on which to base the transaction ID.
-     * @param digestAlgorithm
-     *            the algorithm to use to digest the key
-     * @return the new <tt>TransactionID</tt>
-     */
-    public static TransactionId createTransactionId(PublicKey pubKey,
-	    String digestAlgorithm) {
-	return new TransactionId(pubKey, digestAlgorithm);
-    }
-
-    /**
-     * Creates a new Transaction Id
-     * <p>
-     * Each call to this method will return a different transaction ID.
-     * 
-     * @return the new <tt>TransactionID</tt>
-     */
-    public static TransactionId createTransactionId() {
-	return new TransactionId();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String toString() {
-	try {
-	    return new String(id, Charsets.US_ASCII.name());
-	} catch (UnsupportedEncodingException e) {
-	    throw new RuntimeException(e);
+	private TransactionId() {
+		try {
+			id = Long.toHexString(ID_SOURCE.getAndIncrement()).getBytes(
+					Charsets.US_ASCII.name());
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
 	}
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(Object o) {
-	if (this == o)
-	    return true;
-	if (o == null || getClass() != o.getClass())
-	    return false;
+	/**
+	 * Creates a new Transaction ID
+	 * <p>
+	 * Each call to this method will return the same transaction ID given the
+	 * same parameters.
+	 * 
+	 * @param pubKey
+	 *            the key on which to base the transaction ID.
+	 * @param digestAlgorithm
+	 *            the algorithm to use to digest the key
+	 * @return the new <tt>TransactionID</tt>
+	 */
+	public static TransactionId createTransactionId(PublicKey pubKey,
+			String digestAlgorithm) {
+		return new TransactionId(pubKey, digestAlgorithm);
+	}
 
-	TransactionId that = (TransactionId) o;
+	/**
+	 * Creates a new Transaction Id
+	 * <p>
+	 * Each call to this method will return a different transaction ID.
+	 * 
+	 * @return the new <tt>TransactionID</tt>
+	 */
+	public static TransactionId createTransactionId() {
+		return new TransactionId();
+	}
 
-	return Arrays.equals(id, that.id);
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		try {
+			return new String(id, Charsets.US_ASCII.name());
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-	return Arrays.hashCode(id);
+		TransactionId that = (TransactionId) o;
 
-    }
+		return Arrays.equals(id, that.id);
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(id);
+
+	}
 }
