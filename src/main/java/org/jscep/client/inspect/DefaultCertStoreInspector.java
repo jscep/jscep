54726bacb8ffd4968c1ba55c64ca0a4ffa368ff9
Dave Grant <david@grant.org.uk>
2013-12-13 10:36:39 +0000

    Improve readability of DefaultCertStoreInspector

--- src/main/java/org/jscep/client/inspect/DefaultCertStoreInspector.java
+++ src/main/java/org/jscep/client/inspect/DefaultCertStoreInspector.java
@@ -50,14 +50,31 @@ final class DefaultCertStoreInspector extends AbstractCertStoreInspector {
      	 * {@inheritDoc}
      	 */
 	protected Collection<X509CertSelector> getRecipientSelectors() {
+		boolean digialSignature = false;
+		boolean nonRepudiation = false;
+		boolean keyEncipherment = true;
+		boolean dataEncipherment = false;
+		
 		X509CertSelector keyEncSelector = new X509CertSelector();
 		keyEncSelector.setBasicConstraints(-2);
-		keyEncSelector.setKeyUsage(new boolean[] { false, false, true });
+		keyEncSelector.setKeyUsage(new boolean[] {
+			digitalSignature, 
+			nonRepudiation, 
+			keyEncipherment,
+			dataEncipherment
+		});
+
+		keyEncipherment = false;
+		dataEncipherment = true;
 
 		X509CertSelector dataEncSelector = new X509CertSelector();
 		dataEncSelector.setBasicConstraints(-2);
-		dataEncSelector
-				.setKeyUsage(new boolean[] { false, false, false, true });
+		dataEncSelector.setKeyUsage(new boolean[] {
+			digitalSignature, 
+			nonRepudiation, 
+			keyEncipherment, 
+			dataEncipherment
+		});
 
 		X509CertSelector caSelector = new X509CertSelector();
 		caSelector.setBasicConstraints(0);
