c877f9c8310f927d192230c2f3b653debb070eae
David Grant <david@grant.org.uk>
2015-04-20 19:26:35 +0100

    Updated release script

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
-mvn clean release:clean
-mvn release:prepare
-mvn release:perform
-git pull origin master --tags
-git checkout $(git tag | tail -n1)
-mvn clean install source:jar javadoc:jar com.googlecode.maven-gcu-plugin:maven-gcu-plugin:1.1:upload
+mvn clean release:clean && \
+mvn release:prepare && \
+mvn release:perform && \
+git pull origin master --tags && \
+git checkout $(git tag | tail -n1) && \
+mvn clean install source:jar javadoc:jar com.googlecode.maven-gcu-plugin:maven-gcu-plugin:1.1:upload && \
 git checkout master
